2b79f701ef68a8feb4b17abfd2149e82e98c7971
Ankit Sarwar <ankitsarwar039@gmail.com>
2024-03-06 18:44:31 +0530

    App.js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,46 @@
 
 import Header from "./components/Header";
-import React from "react";
+import React, { useEffect } from "react";
 import Auth from "./components/Auth";
 import Blogs from "./components/Blogs";
 import UserBlogs from "./components/UserBlogs";
 import BlogDetail from "./components/BlogDetail";
 import AddBlog from "./components/AddBlog";
-import {  useSelector } from "react-redux";
+import {  useDispatch ,useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { authActions } from "./store";
 
 function App() {
-  const isLoggedIn = useSelector(state=> state.isLoggedIn);
-  console.log(isLoggedIn);
-  return <React.Fragment>
-    <header>
-       <Header/>
-    </header>
-    <main>
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/add" element={<AddBlog />} />
-        <Route path="/myBlogs" element={<UserBlogs />} />
-        <Route path="/myBlogs/:id" element={<BlogDetail />}/>
-      </Routes>
-    </main>
-  </React.Fragment>  
+  const dispath = useDispatch();
 
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      dispath(authActions.login());
+    }
+  }, [dispath]);
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          {!isLoggedIn ? (
+            <Route path="/auth" element={<Auth />} />
+          ) : (
+            <>
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blogs/add" element={<AddBlog />} />
+              <Route path="/myBlogs" element={<UserBlogs />} />
+              <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+            </>
+          )}
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
-
